2d0b2eae178741845133a003ec7f791478d84913
Dustin Ross <dustinross@live.com>
2022-10-09 19:10:41 -0700

    Added some tests, fixed constructors, made C++20 compatibile

--- mope_vec.h
+++ mope_vec.h
@@ -3,29 +3,73 @@
 #include <type_traits>
 #include <iostream>
 #include <cassert>
-#include <array>
 #include <math.h>
 
 namespace mope
 {
-    /*========================================================================*\
-    |  _base:                                                                  |
-    |  Contains operations common to both vectors and matrices                 |
-    |  (a vector is just a 1xN matrix)                                         |
-    \*========================================================================*/
-    /*
-        N		The dimension of the vector (number of columns for a matrix).
-        T		The inner type of the elements (column vectors for a matrix).
-        Vec     The derived type.
-    */
+    template <size_t N, typename T> struct vec;
+    template <size_t N, typename V> struct mat;
+
+    typedef vec<2, int>				vec2i;
+    typedef vec<3, int>				vec3i;
+    typedef vec<4, int>				vec4i;
+    typedef vec<2, float>			vec2f;
+    typedef vec<3, float>			vec3f;
+    typedef vec<4, float>			vec4f;
+    typedef vec<2, double>			vec2d;
+    typedef vec<3, double>			vec3d;
+    typedef vec<4, double>			vec4d;
+    typedef vec<2, uint32_t>    	vec2ui;
+    typedef vec<3, uint32_t>    	vec3ui;
+    typedef vec<4, uint32_t>	    vec4ui;
+    typedef vec<2, uint8_t>			vec2b;
+    typedef vec<3, uint8_t>			vec3b;
+    typedef vec<4, uint8_t>			vec4b;
+
+    typedef mat<2, vec2i>			mat2i;
+    typedef mat<3, vec3i>			mat3i;
+    typedef mat<4, vec4i>			mat4i;
+    typedef mat<2, vec2f>			mat2f;
+    typedef mat<3, vec3f>			mat3f;
+    typedef mat<4, vec4f>			mat4f;
+    typedef mat<2, vec2d>			mat2d;
+    typedef mat<3, vec3d>			mat3d;
+    typedef mat<4, vec4d>			mat4d;
+    typedef mat<2, vec2ui>			mat2ui;
+    typedef mat<3, vec3ui>			mat3ui;
+    typedef mat<4, vec4ui>			mat4ui;
+
     namespace
     {
+        /*====================================================================*\
+        |  _base:                                                              |
+        |  Contains operations common to both vectors and matrices             |
+        |  (matrices are like row vectors over the field of column vectors)    |
+        |                                                                      |
+        |   N		The dimension (# of columns for a matrix)                  |
+        |   T		The inner type (column vectors for a matrix)               |
+        |   Vec     The derived type (see CRTP)                                |
+        \*====================================================================*/
         template <size_t N, typename T, template <size_t, typename> class Vec>
         struct _base
         {
-            T elements[N] = { };
+            T elements[N]{};
 
             constexpr _base() = default;
+            constexpr _base(const std::initializer_list<T>& L) {
+                assert(L.size() <= N);
+                std::copy(L.begin(), L.end(), elements);
+            }
+
+            // explicit cast to different data type
+            template <typename S>
+            constexpr explicit operator Vec<N, S>() const
+            {
+                Vec<N, S> res;
+                for (size_t i = 0; i < N; ++i)
+                    res[i] = static_cast<S>((*this)[i]);
+                return res;
+            }
 
             // subscript operator
             constexpr T& operator [] (const size_t& i)
@@ -129,60 +173,51 @@ namespace mope
             {
                 return !(*this == other);
             }
-        };
-    }
+        }; // struct _base
 
-    // Scalar left-multiplication. Also explicitly instantiates _base::operator* for valid scalar types.
-    template <size_t N, typename T, template <size_t, typename> class Vec>
-    constexpr auto operator * (int lhs, const _base<N, T, Vec>& rhs)
-    {
-        return rhs * lhs;
-    }
+        // Left-multiplication by int scalar.
+        template <size_t N, typename T, template <size_t, typename> class Vec>
+        constexpr auto operator * (int lhs, const _base<N, T, Vec>& rhs)
+        {
+            return rhs * lhs;
+        }
+        // Left-multiplication by float scalar.
+        template <size_t N, typename T, template <size_t, typename> class Vec>
+        constexpr auto operator * (float lhs, const _base<N, T, Vec>& rhs)
+        {
+            return rhs * lhs;
+        }
+        // Left-multiplication by double scalar.
+        template <size_t N, typename T, template <size_t, typename> class Vec>
+        constexpr auto operator * (double lhs, const _base<N, T, Vec>& rhs)
+        {
+            return rhs * lhs;
+        }
 
-    template <size_t N, typename T, template <size_t, typename> class Vec>
-    constexpr auto operator * (float lhs, const _base<N, T, Vec>& rhs)
-    {
-        return rhs * lhs;
-    }
-
-    template <size_t N, typename T, template <size_t, typename> class Vec>
-    constexpr auto operator * (double lhs, const _base<N, T, Vec>& rhs)
-    {
-        return rhs * lhs;
-    }
-
-    
-    /*========================================================================*\
-    |  vec                                                                     |
-    \*========================================================================*/
-    template <size_t N, typename T> struct vec;
-    namespace
-    {
         template <size_t N, typename T>
         struct _vec : public _base<N, T, vec>
         {
+            using _base<N, T, vec>::_base;
+
             // implicit cast to higher dimensions
-            template <size_t P>
+            template <
+                size_t P,
+                typename PreventCastToLowerDimension = std::enable_if_t<P >= N> >
             constexpr operator vec<P, T>() const
             {
-                static_assert(P >= N, "Can't cast vector to a lower dimension.");
-
                 vec<P, T> res;
                 for (size_t i = 0; i < N; ++i)
                     res[i] = (*this)[i];
                 return res;
             }
 
-            // explicit cast to different data type / size
-            template <size_t P, typename S>
+            // explicit cast to different data type and higher dimension
+            template <
+                size_t P, typename S,
+                typename PreventCastToLowerDimension = std::enable_if_t<P >= N> >
             constexpr explicit operator vec<P, S>() const
             {
-                static_assert(P >= N, "Can't cast vector to a lower dimension.");
-
-                vec<P, S> res;
-                for (size_t i = 0; i < N; ++i)
-                    res[i] = static_cast<S>((*this)[i]);
-                return res;
+                return static_cast<vec<P, S>>(static_cast<vec<N, S>>(*this));
             }
 
             // dot product
@@ -215,30 +250,24 @@ namespace mope
             {
                 vec< N, std::common_type_t<T, S>> res;
                 for (size_t i = 0; i < N; ++i)
-                {
                     res[i] = (*this)[i] * v[i];
-                }
                 return res;
             }
 
             // magnitude
             double magnitude() const
             {
-                double res = 0;
                 vec<N, double> v = static_cast<vec<N, double>>(*this);
-                for (size_t i = 0; i < N; ++i)
-                    res += v[i] * v[i];
-                return sqrt(res);
+                double dot = v.dot(v);
+                return sqrt(dot);
             }
 
             // magnitude as a float
-            float fmagnitude() const
+            float magnitudef() const
             {
-                float res = 0;
                 vec<N, float> v = static_cast<vec<N, float>>(*this);
-                for (size_t i = 0; i < N; ++i)
-                    res += v[i] * v[i];
-                return sqrt(res);
+                float dot = v.dot(v);
+                return sqrtf(dot);
             }
 
             // unit vector
@@ -249,104 +278,17 @@ namespace mope
             }
 
             // unit vector as floats
-            vec<N, float> funit() const
+            vec<N, float> unitf() const
             {
-                float factor = 1.f / this->fmagnitude();
+                float factor = 1.f / this->magnitudef();
                 return (*this) * factor;
             }
-        };
-    }
-    
-    
-    /*========================================================================*\
-    |  vec --- specializations                                                 |
-    \*========================================================================*/
-    template <class T>
-    struct vec<2, T> : public _vec<2, T>
-    {
-        constexpr vec()
-        {
-        }
-        constexpr vec(const T& x, const T& y)
-        {
-            (*this)[0] = x;
-            (*this)[1] = y;
-        }
+        }; // struct _vec
 
-        constexpr T& x() { return (*this)[0]; }
-        constexpr T& y() { return (*this)[1]; }
-        constexpr const T& x() const { return (*this)[0]; }
-        constexpr const T& y() const { return (*this)[1]; }
-    };
-
-    template <class T>
-    struct vec<3, T> : public _vec<3, T>
-    {
-        constexpr vec()
-        {
-        }
-        constexpr vec(const T& x, const T& y, const T& z)
-        {
-            (*this)[0] = x;
-            (*this)[1] = y;
-            (*this)[2] = z;
-        }
-
-        constexpr T& x() { return (*this)[0]; }
-        constexpr T& y() { return (*this)[1]; }
-        constexpr T& z() { return (*this)[2]; }
-        constexpr const T& x() const { return (*this)[0]; }
-        constexpr const T& y() const { return (*this)[1]; }
-        constexpr const T& z() const { return (*this)[2]; }
-    };
-
-    template <class T>
-    struct vec<4, T> : public _vec<4, T>
-    {
-        constexpr vec()
-        {
-        }
-        constexpr vec(const T& x, const T& y, const T& z, const T& w)
-        {
-            (*this)[0] = x;
-            (*this)[1] = y;
-            (*this)[2] = z;
-            (*this)[3] = w;
-        }
-        constexpr vec<4, T>(const vec<4, T>& other)
-        {
-            memcpy(this->elements, other.elements, 4 * sizeof(T));
-        }
-        constexpr vec<4, T>& operator=(const vec<4, T>& other)
-        {
-            memcpy(this->elements, other.elements, 4 * sizeof(T));
-            return *this;
-        }
-
-        constexpr T& x() { return (*this)[0]; }
-        constexpr T& y() { return (*this)[1]; }
-        constexpr T& z() { return (*this)[2]; }
-        constexpr T& w() { return (*this)[3]; }
-        constexpr const T& x() const { return (*this)[0]; }
-        constexpr const T& y() const { return (*this)[1]; }
-        constexpr const T& z() const { return (*this)[2]; }
-        constexpr const T& w() const { return (*this)[3]; }
-    };
-
-    template <size_t N, class T>
-    struct vec : public _vec<N, T>
-    { };
-
-
-    /*========================================================================*\
-    |  mat                                                                    |
-    \*========================================================================*/
-    template <size_t N, typename V> struct mat;
-    namespace
-    {
         template <size_t M, size_t N, typename T>
         struct _mat : public _base<N, vec<M, T>, mat>
         {
+            using _base<N, vec<M, T>, mat>::_base;
             using _base<N, vec<M, T>, mat>::operator*;
 
             // matrix - vector multiplication
@@ -385,26 +327,79 @@ namespace mope
             // get all the column-ordered elements as a vector
             constexpr auto unpack() const
             {
-                std::array<T, M * N> dat;
+                std::array<T, M* N> dat;
                 for (size_t j = 0; j < N; ++j)
                     for (size_t i = 0; i < M; ++i)
                         dat[j * N + i] = (*this)[j][i];
                 return dat;
             }
-        };
-    }
+        }; // struct _mat
+    } // anonymous namespace
 
+    /*========================================================================*\
+    |  vec                                                                     |
+    \*========================================================================*/
+
+    // N-dimensional vector
+    template <size_t N, typename T>
+    struct vec : public _vec<N, T>
+    {
+        using _vec<N, T>::_vec;
+    };
+
+    // 2-dimensional vector
+    template <class T>
+    struct vec<2, T> : public _vec<2, T>
+    {
+        using _vec<2, T>::_vec;
+
+        constexpr T& x() { return (*this)[0]; }
+        constexpr T& y() { return (*this)[1]; }
+        constexpr const T& x() const { return (*this)[0]; }
+        constexpr const T& y() const { return (*this)[1]; }
+    };
+
+    // 3-dimensional vector
+    template <class T>
+    struct vec<3, T> : public _vec<3, T>
+    {
+        using _vec<3, T>::_vec;
+
+        constexpr T& x() { return (*this)[0]; }
+        constexpr T& y() { return (*this)[1]; }
+        constexpr T& z() { return (*this)[2]; }
+        constexpr const T& x() const { return (*this)[0]; }
+        constexpr const T& y() const { return (*this)[1]; }
+        constexpr const T& z() const { return (*this)[2]; }
+    };
+
+    // 4-dimensional vector
+    template <class T>
+    struct vec<4, T> : public _vec<4, T>
+    {
+        using _vec<4, T>::_vec;
+
+        constexpr T& x() { return (*this)[0]; }
+        constexpr T& y() { return (*this)[1]; }
+        constexpr T& z() { return (*this)[2]; }
+        constexpr T& w() { return (*this)[3]; }
+        constexpr const T& x() const { return (*this)[0]; }
+        constexpr const T& y() const { return (*this)[1]; }
+        constexpr const T& z() const { return (*this)[2]; }
+        constexpr const T& w() const { return (*this)[3]; }
+    };
+
+
+    /*========================================================================*\
+    |  mat                                                                     |
+    \*========================================================================*/
+
+    // Column-major matrix class
     template <size_t M, size_t N, typename T>
     struct mat<N, vec<M, T>> : public _mat<M, N, T>
-    { 
-        constexpr mat()
-        {
-        }
-        constexpr mat(const std::initializer_list<vec<M, T>>& L)
-        {
-            assert(L.size() <= N);
-            std::copy(L.begin(), L.end(), this->elements);
-        }
+    {
+        using _mat<M, N, T>::_mat;
+
         constexpr mat(const std::initializer_list<T>& L)
         {
             assert(L.size() <= N * M);
@@ -415,8 +410,7 @@ namespace mope
             }
         }
 
-        using SquareMatrix = typename std::enable_if<M == N, mat<N, vec<N, T>>>::type;
-        constexpr static SquareMatrix identity()
+        constexpr static std::enable_if_t<M == N, mat<N, vec<N, T>>> identity()
         {
             mat<N, vec<N, T>> res{};
             for (size_t i = 0; i < N; ++i)
@@ -425,23 +419,11 @@ namespace mope
         }
     };
 
+    /*========================================================================*\
+    |  Conveniences                                                            |
+    \*========================================================================*/
 
-    // allow for implicit type promotion in vec operations
-    template <size_t N, typename T1, typename T2>
-    struct std::common_type<vec<N, T1>, vec<N, T2>> {
-        using type = mope::vec<N, std::common_type_t<T1, T2>>;
-    };
-    template <size_t N, typename T1, typename T2>
-    struct std::common_type<vec<N, T1>, T2> {
-        using type = mope::vec<N, std::common_type_t<T1, T2>>;
-    };
-    template <size_t N, typename T1, typename T2>
-    struct std::common_type<T1, vec<N, T2>> {
-        using type = mope::vec<N, std::common_type_t<T1, T2>>;
-    };
-
-
-    // stream printing 
+    // Stream printing
     template <size_t N, typename T, template <size_t, typename> class Vec>
     std::ostream& operator<<(std::ostream& os, const _base<N, T, Vec>& v) {
         os << "( ";
@@ -451,6 +433,7 @@ namespace mope
         os << ")";
         return os;
     }
+    // Stream printing (wide)
     template <size_t N, typename T, template <size_t, typename> class Vec>
     std::wostream& operator<<(std::wostream& os, const _base<N, T, Vec>& v) {
         os << "( ";
@@ -461,32 +444,19 @@ namespace mope
         return os;
     }
 
-    typedef vec<2, int>				vec2i;
-    typedef vec<3, int>				vec3i;
-    typedef vec<4, int>				vec4i;
-    typedef vec<2, float>			vec2f;
-    typedef vec<3, float>			vec3f;
-    typedef vec<4, float>			vec4f;
-    typedef vec<2, double>			vec2d;
-    typedef vec<3, double>			vec3d;
-    typedef vec<4, double>			vec4d;
-    typedef vec<2, uint32_t>    	vec2ui;
-    typedef vec<3, uint32_t>    	vec3ui;
-    typedef vec<4, uint32_t>	    vec4ui;
-    typedef vec<2, uint8_t>			vec2b;
-    typedef vec<3, uint8_t>			vec3b;
-    typedef vec<4, uint8_t>			vec4b;
-
-    typedef mat<2, vec2i>			mat2i;
-    typedef mat<3, vec3i>			mat3i;
-    typedef mat<4, vec4i>			mat4i;
-    typedef mat<2, vec2f>			mat2f;
-    typedef mat<3, vec3f>			mat3f;
-    typedef mat<4, vec4f>			mat4f;
-    typedef mat<2, vec2d>			mat2d;
-    typedef mat<3, vec3d>			mat3d;
-    typedef mat<4, vec4d>			mat4d;
-    typedef mat<2, vec2ui>			mat2ui;
-    typedef mat<3, vec3ui>			mat3ui;
-    typedef mat<4, vec4ui>			mat4ui;
+    // common type of two vecs with different inner data types
+    template <size_t N, typename T1, typename T2>
+    struct std::common_type<mope::vec<N, T1>, mope::vec<N, T2>> {
+        using type = mope::vec<N, std::common_type_t<T1, T2>>;
+    };
+    // common type of a vec and a different data type
+    template <size_t N, typename T1, typename T2>
+    struct std::common_type<mope::vec<N, T1>, T2> {
+        using type = mope::vec<N, std::common_type_t<T1, T2>>;
+    };
+    // common type of a different data type and a vec
+    template <size_t N, typename T1, typename T2>
+    struct std::common_type<T1, mope::vec<N, T2>> {
+        using type = mope::vec<N, std::common_type_t<T1, T2>>;
+    };
 } // namespace mope
---
+++ tests/tests.cpp
@@ -0,0 +1,256 @@
+#include "CppUnitTest.h"
+
+#include "../mope_vec.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace mope;
+
+template<> inline std::wstring
+Microsoft::VisualStudio::CppUnitTestFramework::ToString<vec3i> (const vec3i& t) {
+	RETURN_WIDE_STRING(t);
+}
+template<> inline std::wstring
+Microsoft::VisualStudio::CppUnitTestFramework::ToString<vec3d>(const vec3d& t) {
+	RETURN_WIDE_STRING(t);
+}
+template<> inline std::wstring
+Microsoft::VisualStudio::CppUnitTestFramework::ToString<vec3f>(const vec3f& t) {
+	RETURN_WIDE_STRING(t);
+}
+template<> inline std::wstring
+Microsoft::VisualStudio::CppUnitTestFramework::ToString<mat3i>(const mat3i& t) {
+	RETURN_WIDE_STRING(t);
+}
+
+
+namespace tests
+{
+	TEST_CLASS(ConstructorProperties)
+	{
+		template<template <class> class Property, class... Ty>
+		void check_property()
+		{
+			(Assert::IsTrue(Property<Ty>::value), ...);
+		}
+
+		// Plagiarized from:
+		// https://stackoverflow.com/questions/16893992/check-if-type-can-be-explicitly-converted
+		// Someday I will understand it
+		template<typename From, typename To>
+		struct is_explicitly_convertible
+		{
+			template<typename T>
+			static void f(T);
+
+			template<typename F, typename T>
+			static constexpr auto test(int) ->
+				decltype(f(static_cast<T>(std::declval<F>())), true) {
+				return true;
+			}
+
+			template<typename F, typename T>
+			static constexpr auto test(...) -> bool {
+				return false;
+			}
+
+			static bool const value = test<From, To>(0);
+		};
+
+	public:
+		TEST_METHOD(DefaultConstructible)
+		{
+			check_property<std::is_default_constructible, vec3i, mat2i>();
+		}
+
+		TEST_METHOD(CopyConstuctible)
+		{
+			check_property<std::is_copy_constructible, vec3i, mat2i>();
+		}
+
+		TEST_METHOD(MoveConstuctible)
+		{
+			check_property<std::is_move_constructible, vec3i, mat2i>();
+		}
+		
+		TEST_METHOD(CopyAssignable)
+		{
+			check_property<std::is_copy_assignable, vec3i, mat2i>();
+		}
+
+		TEST_METHOD(MoveAssignable)
+		{
+			check_property<std::is_move_assignable, vec3i, mat2i>();
+		}
+
+		TEST_METHOD(ImplicitlyConvertibleToHigherDimensions)
+		{
+			Assert::IsTrue(std::is_convertible_v<vec2i, vec3i>);
+		}
+
+		TEST_METHOD(NotImplicitlyConvertibleToLowerDimensions)
+		{
+			Assert::IsFalse(std::is_convertible_v<vec3i, vec2i>);
+		}
+
+		TEST_METHOD(NotExplicitlyConvertibleToLowerDimensions)
+		{
+			Assert::IsFalse(is_explicitly_convertible<vec3i, vec2i>::value);
+		}
+
+		TEST_METHOD(ExplicitlyConvertibleToOtherType)
+		{
+			Assert::IsTrue(is_explicitly_convertible<vec3i, vec3f>::value);
+			Assert::IsTrue(is_explicitly_convertible<mat3i, mat3f>::value);
+		}
+
+		TEST_METHOD(NotImplicitlyConvertibleToOtherType)
+		{
+			Assert::IsFalse(std::is_convertible_v<vec3i, vec3f>);
+			Assert::IsFalse(std::is_convertible_v<mat3i, mat3f>);
+		}
+
+		TEST_METHOD(ExplicitlyConvertibleToOtherTypeAndHigherDimension)
+		{
+			Assert::IsTrue(is_explicitly_convertible<vec3i, vec4f>::value);
+		}
+	};
+
+	TEST_CLASS(VectorArithmetic)
+	{
+		vec3i u{ 0, 1, 2 };
+		vec3i v{ 1, 2, 3 };
+
+	public:
+		TEST_METHOD(Addition)
+		{
+			vec3i expected{ 1, 3, 5 };
+
+			Assert::AreEqual(expected, u + v);
+			Assert::AreEqual(expected, v + u);
+		}
+
+		TEST_METHOD(Subtraction)
+		{
+			vec3i expected{ 1, 1, 1 };
+
+			Assert::AreEqual(expected, v - u);
+			Assert::AreEqual(-expected, u - v);
+		}
+
+		TEST_METHOD(ScalarMultiplication)
+		{
+			vec3i expected{ 0, 2, 4};
+
+			Assert::AreEqual(expected, 2 * u);
+			Assert::AreEqual(expected, u * 2);
+		}
+
+		TEST_METHOD(DotProduct)
+		{
+			int expected{ 8 };
+
+			Assert::AreEqual(expected, u.dot(v));
+			Assert::AreEqual(expected, v.dot(u));
+		}
+
+		TEST_METHOD(CrossProduct)
+		{
+			vec3i expected{ -1, 2, -1 };
+
+			Assert::AreEqual(expected, u.cross(v));
+		}
+
+		TEST_METHOD(UnitVector)
+		{
+			vec3d actual1{ u.unit() };
+			vec3f actual2{ u.unitf() };
+			
+			double root5 = sqrt(5);
+			float root5f = sqrtf(5);
+			vec3d expected1{ 0.0, 1.0 / root5, 2.0 / root5 };
+			vec3f expected2{ 0.f, 1.f / root5f, 2.f / root5f };
+			
+			Assert::AreEqual(expected1, actual1);
+			Assert::AreEqual(expected2, actual2);
+		}
+	};
+
+	TEST_CLASS(MatrixArithmetic)
+	{
+		mat3i m{
+			0, 1, 2,
+			3, 4, 5,
+			6, 7, 8
+		};
+
+		mat3i n{
+			6, 7, 8,
+			3, 4, 5,
+			0, 1, 2
+		};
+
+		TEST_METHOD(Addition)
+		{
+			mat3i expected{
+				6, 8, 10,
+				6, 8, 10,
+				6, 8, 10
+			};
+
+			Assert::AreEqual(expected, m + n);
+			Assert::AreEqual(expected, n + m);
+		}
+
+		TEST_METHOD(Subtraction)
+		{
+			mat3i expected{
+				6, 6, 6,
+				0, 0, 0,
+				-6, -6, -6
+			};
+
+			Assert::AreEqual(expected, n - m);
+			Assert::AreEqual(-expected, m - n);
+		}
+
+		TEST_METHOD(MatrixMatrixMultiplication)
+		{
+			mat3i expected{
+				69, 90, 111,
+				42, 54, 66,
+				15, 18, 21
+			};
+
+			Assert::AreEqual(expected, m * n);
+		}
+
+		TEST_METHOD(MatrixVectorMultiplication)
+		{
+			vec3i v{ 1, 2, 3 };
+			vec3i expected{
+				24, 30, 36
+			};
+
+			Assert::AreEqual(expected, m * v);
+		}
+
+		TEST_METHOD(Identity)
+		{
+			mat3i I = mat3i::identity();
+
+			Assert::AreEqual(m, m * I);
+			Assert::AreEqual(m, I * m);
+		}
+
+		TEST_METHOD(Transpose)
+		{
+			mat3i expected = {
+				0, 3, 6,
+				1, 4, 7,
+				2, 5, 8
+			};
+
+			Assert::AreEqual(expected, m.transpose());
+		}
+	};
+}
