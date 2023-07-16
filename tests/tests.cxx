bb32116ea33893138484c246ab260b8bee160c7f
Dustin Ross <dustinross@live.com>
2023-07-16 16:51:34 -0400

    test cases: equality, construction, arithmetic

--- tests/tests.cxx
+++ tests/tests.cxx
@@ -3,216 +3,199 @@
 
 #include <type_traits>
 
-TEST_CASE( "vecs and mats can be constructed and cast", "[construction]") {
-    SECTION( "default constructible" ) {
-        REQUIRE( std::is_default_constructible_v<mope::vec2i> );
-        REQUIRE( std::is_default_constructible_v<mope::mat2i> );
-    }
+TEST_CASE( "vecs and mats have desired type traits", "[type traits]") {
+
+	SECTION( "vecs and mats are default constructible" ) {
+		REQUIRE( std::is_default_constructible_v<mope::vec2i> );
+		REQUIRE( std::is_default_constructible_v<mope::mat2i> );
+	}
+
 	SECTION( "vecs and mats are copy constructible" ) {
-        REQUIRE( std::is_copy_constructible_v<mope::vec2i> );
-        REQUIRE( std::is_copy_constructible_v<mope::mat2i> );
-    }
+		REQUIRE( std::is_copy_constructible_v<mope::vec2i> );
+		REQUIRE( std::is_copy_constructible_v<mope::mat2i> );
+	}
+
 	SECTION( "vecs and mats copy assignable" ) {
-        REQUIRE( std::is_copy_assignable_v<mope::vec2i> );
-        REQUIRE( std::is_copy_assignable_v<mope::mat2i> );
-    }
+		REQUIRE( std::is_copy_assignable_v<mope::vec2i> );
+		REQUIRE( std::is_copy_assignable_v<mope::mat2i> );
+	}
+
 	SECTION( "vecs and mats move constructible" ) {
-        REQUIRE( std::is_move_constructible_v<mope::vec2i> );
-        REQUIRE( std::is_move_constructible_v<mope::mat2i> );
-    }
+		REQUIRE( std::is_move_constructible_v<mope::vec2i> );
+		REQUIRE( std::is_move_constructible_v<mope::mat2i> );
+	}
+
 	SECTION( "vecs and mats move assignable" ) {
-        REQUIRE( std::is_move_assignable_v<mope::vec2i> );
-        REQUIRE( std::is_move_assignable_v<mope::mat2i> );
-    }
+		REQUIRE( std::is_move_assignable_v<mope::vec2i> );
+		REQUIRE( std::is_move_assignable_v<mope::mat2i> );
+	}
+
 	SECTION( "vecs are convertible from lower dimensions" ) {
-        REQUIRE( std::is_convertible_v<mope::vec2i, mope::vec3i> );
-    }
+		REQUIRE( std::is_convertible_v<mope::vec2i, mope::vec3i> );
+	}
+
 	SECTION( "vecs are not convertible from higher dimensions" ) {
-        REQUIRE_FALSE( std::is_constructible_v<mope::vec2i, mope::vec3i> );
-    }
+		REQUIRE_FALSE( std::is_constructible_v<mope::vec2i, mope::vec3i> );
+	}
+
 	SECTION( "vecs and mats are explicitly convertible to another field type" ) {
-        REQUIRE_FALSE( std::is_convertible_v<mope::vec2i, mope::vec2f> );
+		REQUIRE_FALSE( std::is_convertible_v<mope::vec2i, mope::vec2f> );
 		REQUIRE( std::is_constructible_v<mope::vec2f, mope::vec2i> );
+	}
+}
+
+TEST_CASE( "preliminary conditions for testing are met", "[preliminary]") {
+
+	constexpr mope::vec2i x{ 1, 2 };
+	constexpr mope::mat2i A{ 1, 3, 2, 4 };
+
+	SECTION( "basic vec construction works as expected" ) {
+		auto dat = x.data( );
+		REQUIRE( dat[0] == 1 );
+		REQUIRE( dat[1] == 2 );
+	}
+
+    SECTION( "vec equality and inequality work as expected" ) {
+		constexpr mope::vec2i y{ 1, 2 };
+        constexpr mope::vec2i z1{ 2, 2 };
+        constexpr mope::vec2i z2{ 1, 1 };
+        REQUIRE( x == y );
+		REQUIRE_FALSE( x == z1 );
+		REQUIRE_FALSE( x == z2 );
+		REQUIRE( x != z1 );
+		REQUIRE( x != z2 );
+		REQUIRE_FALSE( x != y );
+	}
+
+	SECTION( "basic mat construction works as expected" ) {
+        constexpr mope::vec2i expected0{ 1, 3 };
+        constexpr mope::vec2i expected1{ 2, 4 };
+        auto dat = A.data( );
+        REQUIRE( dat[0] == expected0 );
+        REQUIRE( dat[1] == expected1 );
     }
+
+	SECTION( "mat equality and inequality work as expected" ) {
+		constexpr mope::mat2i B{ 1, 3, 2, 4 };
+        constexpr mope::mat2i C1{ 1, 3, 1, 3 };
+        constexpr mope::mat2i C2{ 2, 4, 2, 4 };
+        REQUIRE( A == B );
+		REQUIRE_FALSE( A == C1 );
+		REQUIRE_FALSE( A == C2 );
+		REQUIRE( A != C1 );
+		REQUIRE( A != C2 );
+		REQUIRE_FALSE( A != B );
+	}
+
+	SECTION( "packed and unpacked mat construction give the same results" ) {
+        constexpr mope::mat2i B{
+            { 1, 3 },
+            { 2, 4 }
+        };
+        REQUIRE( A == B );
+    }
+
+    SECTION( "copying and moving work as expected") {
+
+		constexpr mope::vec2i x_expected{ 1, 2 };
+		constexpr mope::mat2i A_expected{ 1, 3, 2, 4 };
+
+		SECTION( "copying construction works as expected" ) {
+			constexpr mope::vec2i x_copy( x );
+			constexpr mope::mat2i A_copy( A );
+			REQUIRE( x == x_expected );
+			REQUIRE( A == A_expected );
+			REQUIRE( x == x_copy );
+			REQUIRE( A == A_copy );
+		}
+
+		SECTION( "copy assignment works as expected" ) {
+			constexpr mope::vec2i x_copy = x;
+			constexpr mope::mat2i A_copy = A;
+			REQUIRE( x == x_expected );
+			REQUIRE( A == A_expected );
+			REQUIRE( x == x_copy );
+			REQUIRE( A == A_copy );
+		}
+
+		SECTION( "move construction works as expected" ) {
+			constexpr mope::vec2i x_move = std::move( x );
+			constexpr mope::mat2i A_move = std::move( A );
+			REQUIRE( x_move == x_expected );
+			REQUIRE( A_move == A_expected );
+		}
+
+		SECTION( "move assignment works as expected" ) {
+			constexpr mope::vec2i x_move{ std::move( x ) };
+			constexpr mope::mat2i A_move{ std::move( A ) };
+			REQUIRE( x_move == x_expected );
+			REQUIRE( A_move == A_expected );
+		}
+	}
+}
+
+TEST_CASE( "vector arithmetic works as expected", "[vector arithmetic]" ) {
+    constexpr mope::vec3i x{ 0, 1, 2 };
+    constexpr mope::vec3i y{ 1, 2, 3 };
+	mope::vec3i w = x;
+
+    SECTION( "addition is correct" ) {
+		constexpr mope::vec3i expected{ 1, 3, 5 };
+		REQUIRE(x + y == expected);
+		REQUIRE(y + x == expected);
+	}
+
+	SECTION( "addition-assignment is correct" ) {
+        constexpr mope::vec3i expected{ 1, 3, 5 };
+        w += y;
+        REQUIRE( w == expected );
+    }
+
+    SECTION( "subtraction is correct" ) {
+		constexpr mope::vec3i expected0{ 1, 1, 1 };
+		constexpr mope::vec3i expected1{ -1, -1, -1 };
+		REQUIRE(y - x == expected0);
+		REQUIRE(x - y == expected1);
+	}
+
+	SECTION( "subtraction-assignment is correct" ) {
+        constexpr mope::vec3i expected{ -1, -1, -1 };
+        w -= y;
+        REQUIRE( w == expected );
+    }
+	
+	SECTION( "negation is correct" ) {
+		constexpr mope::vec3i expected{ -1, -2, -3 };
+		REQUIRE(-y == expected);
+	}
+
+	SECTION( "scalar multiplication is correct" ) {
+		constexpr mope::vec3i expected{ 0, 2, 4};
+		REQUIRE(2 * x == expected);
+		REQUIRE(x * 2 == expected);
+	}
+
+	SECTION( "dot product is correct" ) {
+		constexpr int expected{ 8 };
+		REQUIRE(x.dot(y) == expected);
+		REQUIRE(y.dot(x) == expected);
+	}
+
+	SECTION( "cross product is correct" ) {
+		constexpr mope::vec3i expected{ -1, 2, -1 };
+		REQUIRE(x.cross(y) == expected);
+	}
+
+	SECTION( "unit vector is correct" ) {	
+		const mope::vec3d expected{ 0.0, 1.0 / std::sqrt(5), 2.0 / std::sqrt(5) };
+		const mope::vec3f expectedf{ 0.f, 1.f / std::sqrt(5.f), 2.f / std::sqrt(5.f) };
+		REQUIRE(x.unit() == expected);
+		REQUIRE(x.unitf() == expectedf);
+	}
 }
 
 /*
-#include "CppUnitTest.h"
 
-#include "../mope_vec.hxx"
-
-using namespace Microsoft::VisualStudio::CppUnitTestFramework;
-using namespace mope;
-
-template<> inline std::wstring
-Microsoft::VisualStudio::CppUnitTestFramework::ToString<vec3i> (const vec3i& t) {
-	RETURN_WIDE_STRING(t);
-}
-template<> inline std::wstring
-Microsoft::VisualStudio::CppUnitTestFramework::ToString<vec3d>(const vec3d& t) {
-	RETURN_WIDE_STRING(t);
-}
-template<> inline std::wstring
-Microsoft::VisualStudio::CppUnitTestFramework::ToString<vec3f>(const vec3f& t) {
-	RETURN_WIDE_STRING(t);
-}
-template<> inline std::wstring
-Microsoft::VisualStudio::CppUnitTestFramework::ToString<mat3i>(const mat3i& t) {
-	RETURN_WIDE_STRING(t);
-}
-
-
-namespace tests
-{
-	TEST_CLASS(ConstructorProperties)
-	{
-		template<template <class> class Property, class... Ty>
-		void check_property()
-		{
-			(Assert::IsTrue(Property<Ty>::value), ...);
-		}
-
-		// Plagiarized from:
-		// https://stackoverflow.com/questions/16893992/check-if-type-can-be-explicitly-converted
-		// Someday I will understand it
-		template<typename From, typename To>
-		struct is_explicitly_convertible
-		{
-			template<typename T>
-			static void f(T);
-
-			template<typename F, typename T>
-			static constexpr auto test(int) ->
-				decltype(f(static_cast<T>(std::declval<F>())), true) {
-				return true;
-			}
-
-			template<typename F, typename T>
-			static constexpr auto test(...) -> bool {
-				return false;
-			}
-
-			static bool const value = test<From, To>(0);
-		};
-
-	public:
-		TEST_METHOD(DefaultConstructible)
-		{
-			check_property<std::is_default_constructible, vec3i, mat2i>();
-		}
-
-		TEST_METHOD(CopyConstuctible)
-		{
-			check_property<std::is_copy_constructible, vec3i, mat2i>();
-		}
-
-		TEST_METHOD(MoveConstuctible)
-		{
-			check_property<std::is_move_constructible, vec3i, mat2i>();
-		}
-		
-		TEST_METHOD(CopyAssignable)
-		{
-			check_property<std::is_copy_assignable, vec3i, mat2i>();
-		}
-
-		TEST_METHOD(MoveAssignable)
-		{
-			check_property<std::is_move_assignable, vec3i, mat2i>();
-		}
-
-		TEST_METHOD(ImplicitlyConvertibleToHigherDimensions)
-		{
-			Assert::IsTrue(std::is_convertible_v<vec2i, vec3i>);
-		}
-
-		TEST_METHOD(NotImplicitlyConvertibleToLowerDimensions)
-		{
-			Assert::IsFalse(std::is_convertible_v<vec3i, vec2i>);
-		}
-
-		TEST_METHOD(NotExplicitlyConvertibleToLowerDimensions)
-		{
-			Assert::IsFalse(is_explicitly_convertible<vec3i, vec2i>::value);
-		}
-
-		TEST_METHOD(ExplicitlyConvertibleToOtherType)
-		{
-			Assert::IsTrue(is_explicitly_convertible<vec3i, vec3f>::value);
-			Assert::IsTrue(is_explicitly_convertible<mat3i, mat3f>::value);
-		}
-
-		TEST_METHOD(NotImplicitlyConvertibleToOtherType)
-		{
-			Assert::IsFalse(std::is_convertible_v<vec3i, vec3f>);
-			Assert::IsFalse(std::is_convertible_v<mat3i, mat3f>);
-		}
-
-		TEST_METHOD(ExplicitlyConvertibleToOtherTypeAndHigherDimension)
-		{
-			Assert::IsTrue(is_explicitly_convertible<vec3i, vec4f>::value);
-		}
-	};
-
-	TEST_CLASS(VectorArithmetic)
-	{
-		vec3i u{ 0, 1, 2 };
-		vec3i v{ 1, 2, 3 };
-
-	public:
-		TEST_METHOD(Addition)
-		{
-			vec3i expected{ 1, 3, 5 };
-
-			Assert::AreEqual(expected, u + v);
-			Assert::AreEqual(expected, v + u);
-		}
-
-		TEST_METHOD(Subtraction)
-		{
-			vec3i expected{ 1, 1, 1 };
-
-			Assert::AreEqual(expected, v - u);
-			Assert::AreEqual(-expected, u - v);
-		}
-
-		TEST_METHOD(ScalarMultiplication)
-		{
-			vec3i expected{ 0, 2, 4};
-
-			Assert::AreEqual(expected, 2 * u);
-			Assert::AreEqual(expected, u * 2);
-		}
-
-		TEST_METHOD(DotProduct)
-		{
-			int expected{ 8 };
-
-			Assert::AreEqual(expected, u.dot(v));
-			Assert::AreEqual(expected, v.dot(u));
-		}
-
-		TEST_METHOD(CrossProduct)
-		{
-			vec3i expected{ -1, 2, -1 };
-
-			Assert::AreEqual(expected, u.cross(v));
-		}
-
-		TEST_METHOD(UnitVector)
-		{
-			vec3d actual1{ u.unit() };
-			vec3f actual2{ u.unitf() };
-			
-			double root5 = sqrt(5);
-			float root5f = sqrtf(5);
-			vec3d expected1{ 0.0, 1.0 / root5, 2.0 / root5 };
-			vec3f expected2{ 0.f, 1.f / root5f, 2.f / root5f };
-			
-			Assert::AreEqual(expected1, actual1);
-			Assert::AreEqual(expected2, actual2);
-		}
-	};
 
 	TEST_CLASS(MatrixArithmetic)
 	{
